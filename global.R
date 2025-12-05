e58b87968eb9b0dfcef667357a997f7085cd8d82
dwadelson <dwadelson@users.noreply.github.com>
2025-12-04 20:52:51 -0800

    added server library paths

--- global.R
+++ global.R
@@ -6,6 +6,13 @@ library(dplyr)
 
 # Set this to TRUE when debugging on the server
 DIAGNOSTICS <- TRUE
+
+#on the server, point to the up to date libraries
+shared_lib <- "/home/elac/shiny_public/libraries_4.4"
+if (dir.exists(shared_lib)) {
+  .libPaths(c(shared_lib, .libPaths()))
+}
+
 # Source the hemodynamic model (defines compute_hemo, baseline_hemo, Pv, baselinePa, dref)
 source("model.R")
 
